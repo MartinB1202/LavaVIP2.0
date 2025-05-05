3174eb695cf55e79a0a141810e8256a409a2682d
MartinB1202 <18258610@uagro.mx>
2025-05-05 15:16:23 -0600

    Create estilosDArt.js

---
+++ Estilos/estilosDArt.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function estilosDArt() {
+  return (
+    <View>
+      <Text>estilosDArt</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
