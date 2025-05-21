81a5393c5eb44a3e2158049f621dd19e7eab64b0
MartinB1202 <18258610@uagro.mx>
2025-05-21 13:52:33 -0600

    Update CartadeCarrito.js

--- componentes/CartadeCarrito.js
+++ componentes/CartadeCarrito.js
@@ -1,23 +1,54 @@
 import React from "react";
 import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
 import FontAwesome6Icon from "react-native-vector-icons/FontAwesome6";
+import { useCarrito } from "./ContextoCarro";
 
-export default function CartadeCarrito({item, carga}){
 
-return(
+export default function CartadeCarrito({item, carga, index}){
+
+    const {eliminarDelCarrito} = useCarrito();
+    const obtenerPrecio = ()=>{
+        switch(carga){
+            case item.CargaP:
+                return item.precioProd;
+            case item.Individual:
+                return item.PrecioInd
+            case item.CargaM:
+                return item.precioProd;
+            case item.Matrimonial:
+                return item.PrecioMat;
+            case item.CargaG:
+                return item.precioProd;
+            case item.Kingsize:
+                return item.PrecioKing;
+            case item.Tenis:
+                return item.PrecioT;
+            case item.Zapatos:
+                return item.PrecioZ;
+            case item.Botas:
+                return item.PrecioB
+            
+        }
+
+    };
+
+    const PrecioFinal = obtenerPrecio();
+
+    return(
     <View style={styles.contenedor}>
         <Image source={{uri: item.imagen}} style={styles.imagen}></Image>
         
 
         <View style={ styles.contenedorT}>
            <Text style={styles.titulo}>{item.tipoProd}</Text> 
-           <Text style={styles.precio}>$ {item.precioProd}.00</Text>
+           <Text style={styles.precio}>$ {PrecioFinal}.00</Text>
+           
            <Text>{carga}</Text>
         </View>
-
+        
         <TouchableOpacity>
         <FontAwesome6Icon name="trash-can" color={'red'} 
-        size={24} />
+        size={24} onPress={()=>eliminarDelCarrito(index)} />
         </TouchableOpacity>
     </View>
 
