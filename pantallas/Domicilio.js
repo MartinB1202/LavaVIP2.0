b9be11ca3289af0f33a3177f0099ff20ce9c1ff8
MartinB1202 <18258610@uagro.mx>
2025-05-21 13:53:11 -0600

    Update Domicilio.js

--- pantallas/Domicilio.js
+++ pantallas/Domicilio.js
@@ -1,15 +1,78 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { Component, useState, useEffect,useCallback } from "react";
+import {StyleSheet, View, Image, TouchableOpacity,  TextInput, Text, Alert, ScrollView, FlatList} from "react-native";
+import { onAuthStateChanged, getAuth, signOut } from "firebase/auth";
+import { doc, getDoc, getDocs, getFirestore, query, collection, where, orderBy, limit } from "firebase/firestore";
+import appfirebase from "../credenciales";
+import Icon from 'react-native-vector-icons/Ionicons'
+import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { LinearGradient } from "expo-linear-gradient";
+import styles from "../Estilos/estilosUsuario";
+import CartaPedido from "../componentes/CartaPedido";
+import { useFocusEffect } from "@react-navigation/native";
 
-const Domicilio = () => {
-  return (
-    <View>
-      <Text>Domicilio</Text>
-    </View>
-  )
+//Obtiene 
+const db = getFirestore(appfirebase);
+const auth = getAuth(appfirebase);
+
+
+export default function Domicilio(){
+
+const[datosPedido, setDatosPedido] = useState([]);
+
+useFocusEffect(
+  useCallback(() => {
+    const obtenerPedidos = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, 'pedidos'));
+        const docs = [];
+        querySnapshot.forEach((doc) => {
+          docs.push({ ...doc.data(), id: doc.id });
+        });
+        setDatosPedido(docs); // esta debe ser tu función de estado
+      } catch (error) {
+        console.log('Error al obtener pedidos:', error);
+      }
+    };
+
+    obtenerPedidos();
+  }, [])
+);
+
+
+
+
+
+
+
+const cerrarS = async ()=>{
+    try{
+        signOut(auth);
+
+    }catch(error){
+      console.log(error);
+    }
+
+
 }
 
-export default Domicilio
 
-const styles = StyleSheet.create({})
+
+    return (
+
+    
+
+    
+<FlatList
+  data={datosPedido}
+  keyExtractor={(item, index) => item.id}
+  renderItem={({ item }) => 
+  <CartaPedido item={item}/>}
+  ListEmptyComponent={<Text>No hay pedidos aún</Text>}
+/>
+
+
+    );
+}
+
 
