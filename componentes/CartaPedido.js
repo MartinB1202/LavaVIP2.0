508c70707441620e05e8d558589b19658cd7bd12
MartinB1202 <18258610@uagro.mx>
2025-05-21 14:41:55 -0600

    Update CartaPedido.js

--- componentes/CartaPedido.js
+++ componentes/CartaPedido.js
@@ -2,7 +2,9 @@ import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity, Platform } from 'react-native';
 import { domoloc } from "../funciones/funCartaPedido"; 
-
+import FontAwesome6Icon from "react-native-vector-icons/FontAwesome6";
+import { deleteDoc, doc } from 'firebase/firestore';
+import { db } from '../credenciales';
 import styles from '../Estilos/estilosCPedido';
 
 
@@ -11,8 +13,17 @@ import styles from '../Estilos/estilosCPedido';
 
 
 export default function CartaPedido({item}) {
- 
 
+
+  
+const elminarPedido = async(idPedido)=>{
+  try{
+    await deleteDoc(doc(db,'pedidos', idPedido));
+
+  }catch(error){
+    console.log('Error al eliminar el pedido', error);
+  }
+};
 
   return (
 
@@ -20,7 +31,7 @@ export default function CartaPedido({item}) {
  <View>
         
         <View style={styles.contenedor}>
-        <Text style={styles.titulo}>{item.productos[0].carga}</Text>
+        <Text style={styles.titulo}>Resumen de pedido</Text>
         <View style={styles.contenedorImagen}>
         <Image style={styles.covertura} 
         source={{uri:item.productos[0].imagen}} 
@@ -29,13 +40,26 @@ export default function CartaPedido({item}) {
         </View>
 
         <View style={styles.contenido}>
-        
-        <Text>Total: ${item.total}</Text>
+         <Text>Resumen del Pedido</Text> 
+          {item.productos.map((producto, i) => (
+        <View key={i}>
+          <View>
+            <Text>Carga: {producto.carga}</Text>
+          </View>
+        </View>
+      ))}
         <Text>Tipo de Entrega: {item.entrega}</Text>
         <Text>{domoloc(item)}</Text>
         <Text>Estado del pedido: {item.estado}</Text>
-        
+        <Text>Total: ${item.total}</Text>
         </View>
+    
+    <TouchableOpacity style={styles.borrar}>
+            <FontAwesome6Icon name="trash-can" color={'red'} 
+            size={24} 
+            onPress={()=>elminarPedido(item.id)} />
+            </TouchableOpacity>
+    
     </View>
     
 
